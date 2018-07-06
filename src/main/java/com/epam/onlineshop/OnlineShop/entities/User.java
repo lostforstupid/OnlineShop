4f545670cfd95ba11dbd5c36e158268b6ae75f9f
lostforstupid <lostforstupid@gmail.com>
2018-07-06 21:02:10 +0300

    added h2 and User entity

---
+++ src/main/java/com/epam/onlineshop/OnlineShop/entities/User.java
@@ -0,0 +1,32 @@
+package com.epam.onlineshop.OnlineShop.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private long id;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "isBlocked")
+    private boolean isBlocked;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+}
