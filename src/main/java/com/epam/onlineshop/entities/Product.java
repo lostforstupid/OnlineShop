72028b6155bf0b9d2e4e93d05163bc73522a7800
lostforstupid <lostforstupid@gmail.com>
2018-07-09 16:16:30 +0300

    added product stuff

---
+++ src/main/java/com/epam/onlineshop/entities/Product.java
@@ -0,0 +1,29 @@
+package com.epam.onlineshop.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+}
