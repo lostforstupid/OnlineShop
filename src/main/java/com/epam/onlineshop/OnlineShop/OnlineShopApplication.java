00df8ed5cc77d1824bef96e5236bf760e6e83f0a
johnnybarden <johnnybarden@yandex.ru>
2018-07-09 12:57:58 +0300

    README.md updated.

--- src/main/java/com/epam/onlineshop/OnlineShop/OnlineShopApplication.java
+++ src/main/java/com/epam/onlineshop/OnlineShop/OnlineShopApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.springframework.boot.SpringApplication.*;
 
-
 @SpringBootApplication
 public class OnlineShopApplication {
 
