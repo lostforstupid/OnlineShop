2d2ce62f05ee51068a250846cc08d7b88b213d7c
lostforstupid <lostforstupid@gmail.com>
2018-07-10 17:01:34 +0300

    added some sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
 INSERT INTO user VALUES ('1', 'Address', false, 'pass', 'ADMIN', 'admin');
+
+INSERT INTO product VALUES ('0', 'Noses (20 per pack)', 7500);
+INSERT INTO product VALUES ('1', 'Browser', 400);
+INSERT INTO product VALUES ('2', 'Fun tea', 38);
+INSERT INTO product VALUES ('3', 'Door', 1400);
