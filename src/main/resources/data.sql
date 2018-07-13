bb15a960c62faca1c902309a4b93bf19b73d3827
johnnybarden <38405488+johnnybarden@users.noreply.github.com>
2018-07-13 18:46:26 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,8 @@
-//<<<<<<< OrderEntity
 INSERT INTO customer (id, role, username, password, is_blocked, address) VALUES
-    (1, 'ADMIN', 'admin', 'pass', 'false', 'Address');
+    (1, 'ADMIN', 'admin', '$2a$10$OUwX3BY0RWAA71PjsLHvwu6CwnqKsqQMVBV/FNywy1a0xMvkg0NAS', 'false', 'Address');
 
 INSERT INTO product (id, name, price, image_url) VALUES
     (0, 'Noses (20 per pack)', 7500, 'girl1.jpg'),
     (1, 'Browser', 400,'girl2.jpg'),
     (2, 'Fun tea', 38,'girl3.jpg'),
     (3,'Door', 1400, 'gallery1.jpg');
-//This conflict is to be resolved.
-//=======
-//INSERT INTO user VALUES (default, 'Address', false, '$2a$10$OUwX3BY0RWAA71PjsLHvwu6CwnqKsqQMVBV/FNywy1a0xMvkg0NAS', 'ADMIN', 'user12345');
-
-//INSERT INTO product VALUES (default, 12, 'girl1.jpg', 'Noses (20 per pack)', 7500);
-//INSERT INTO product VALUES (default,23, 'girl2.jpg', 'Browser', 400);
-//INSERT INTO product VALUES (default, 122, 'girl3.jpg', 'Fun tea', 38);
-//INSERT INTO product VALUES (default, 34, 'gallery1.jpg','Door', 1400);
-//>>>>>>> master
