24478d3e14b878af9f247805fc1c3e696361f636
johnnybarden <johnnybarden@yandex.ru>
2018-07-13 16:48:52 +0300

    Product in order entity added. Hibernate connections added. Tables' names modified.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,12 +3,9 @@ INSERT INTO customer (id, role, username, password, is_blocked, address) VALUES
     (1, 'ADMIN', 'admin', 'pass', 'false', 'Address');
 
 INSERT INTO product (id, name, price, image_url) VALUES
-    (0, 'Noses (20 per pack)', 7500, 'girl1.jpg');
-INSERT INTO product (id, name, price, image_url) VALUES
-    (1, 'Browser', 400,'girl2.jpg');
-INSERT INTO product (id, name, price, image_url) VALUES
-    (2, 'Fun tea', 38,'girl3.jpg');
-INSERT INTO product (id, name, price, image_url) VALUES
+    (0, 'Noses (20 per pack)', 7500, 'girl1.jpg'),
+    (1, 'Browser', 400,'girl2.jpg'),
+    (2, 'Fun tea', 38,'girl3.jpg'),
     (3,'Door', 1400, 'gallery1.jpg');
 //=======
 //INSERT INTO user VALUES (default, 'Address', false, '$2a$10$OUwX3BY0RWAA71PjsLHvwu6CwnqKsqQMVBV/FNywy1a0xMvkg0NAS', 'ADMIN', 'user12345');
