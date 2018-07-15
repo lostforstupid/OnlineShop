96d134cb94c657acafb2e925c2247c4b5c74b2ae
Gradder <rzhitkov@gmail.com>
2018-07-15 14:11:36 +0300

    temporary count disabled

--- src/main/java/com/epam/onlineshop/controllers/ProductController.java
+++ src/main/java/com/epam/onlineshop/controllers/ProductController.java
@@ -34,7 +34,7 @@ public class ProductController {
         String name = String.valueOf(currentTime);
         catalog = ImageWriter.writeImage(catalog, file, name);
         product.setImageLink(name + ".jpg");
-        product.setCount(100); //TEMPORARY
+        //product.setCount(100); //TEMPORARY
         productService.addNewProduct(product);
         catalog.addObject(productService.getAllProducts());
         return catalog;
